7c264497f767db754c7b9925219dd3ea877a6d7d
PrajnaShetty <shetty.prajna86@gmail.com>
2015-03-19 23:57:44 -0700

    bug fix & new method

--- src/com/prajna/demo/MultiNodeAppWithoutLock.java
+++ src/com/prajna/demo/MultiNodeAppWithoutLock.java
@@ -1,15 +1,12 @@
 package com.prajna.demo;
 
-import java.util.concurrent.TimeUnit;
-
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.ICountDownLatch;
 import com.hazelcast.core.IMap;
 
 
-public class MultiNodeApplication {
+public class MultiNodeAppWithoutLock {
 	
 	 public static void main( String[] args ) throws InterruptedException {
 		 
@@ -22,20 +19,14 @@ public class MultiNodeApplication {
 	     if (!printStatusMap.containsKey("printStatus"))
 	    	 printStatusMap.put("printStatus", false);	     
 	     else{
-	    	 	//obtaining lock	    	 
-				if (printStatusMap.tryLock("printStatus", 5000, TimeUnit.MILLISECONDS)) {
-					try {
-						 if (printStatusMap.get("printStatus") == false)
-						 {
-							 System.out.println("We are started !!");	
-							 printStatusMap.put("printStatus", true);
-						 }
-					}
-				  finally{
-							 printStatusMap.unlock("printStatus");
-					}
-					
-				}	    	 
+	    	 Thread.sleep(10);
+	    	 boolean oldValue = printStatusMap.get("printStatus");
+	    	 if (oldValue  == false)
+			 {				 	
+	    		 boolean newValue = true;
+				 printStatusMap.replace("printStatus", oldValue, newValue); // atomic operation
+				 System.out.println("We are started !! (Without Lock)");
+			 }
 	     }	     	     
 	 }
 }
